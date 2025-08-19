a0c90ddf6afb097c5721cf14729eaad869c54a5f
marcos-j-ferreira <marcos.j.lemes.2004@gmal.com>
2025-08-19 15:36:12 -0300

    feat: initial settings in mysql with docker

---
+++ db/settings.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir -p ./mysql-data
+
+set -e
+
+
+docker run -d \
+  --name dataBase-ecommerce \
+  -e MYSQL_ROOT_PASSWORD=senha123 \
+  -e MYSQL_DATABASE=system-01 \
+  -e MYSQL_USER=marcos \
+  -e MYSQL_PASSWORD=senha123 \
+  -v $(pwd)/mysql-data:/var/lib/mysql \
+  -p 3306:3306 \
+  --restart unless-stopped \
+  mysql:8.0.30
+
+
+  # -v $(pwd)/mysql-config:/etc/mysql/conf.d \
